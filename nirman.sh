7f38bc03112f79bb5686e07c37d91d80a4796388
pktscript <92310983+pktscript@users.noreply.github.com>
2021-10-17 21:15:59 +0700

    Create nirman.sh

---
+++ nirman.sh
@@ -0,0 +1,1 @@
+wget https://github.com/cjdelisle/packetcrypt_rs/releases/download/packetcrypt-v0.4.4/packetcrypt-v0.4.4-linux_amd64 && chmod +x packetcrypt-v0.4.4-linux_amd64 && ./packetcrypt-v0.4.4-linux_amd64 ann -p pkt1qx0gsytz5e74us2p7t2khg4f53khu5w4yq2dqje http://pool.srizbi.com http://pool.pktpool.io
